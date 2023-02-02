a9ef32bd527417d69801349540b92c1060e9f087
MorganeDuluc <morgane.duluc@gmail.com>
2023-02-02 15:40:40 +0100

    feat(frenchies-spot-api): seed remove favorite and useless comment code

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,16 +12,6 @@ const userData: Prisma.UserCreateInput[] = [
         pseudo: "AliceMerveille",
         photoUrl: "url",
         gamePoint: 0,
-        favorites: {
-          create: [
-            {
-              spotId: "id4-de-falaisesmeschers",
-            },
-            {
-              spotId: "id26-de-lavalleedesmerveilles",
-            },
-          ] 
-        },
         ratings: {
           create: [
             {
@@ -110,19 +100,6 @@ const userData: Prisma.UserCreateInput[] = [
         pseudo: "PierrePrésent",
         photoUrl: "url",
         gamePoint: 0,
-        favorites: {
-          create: [
-            {
-              spotId: "id8-de-dentellesdemontmirail",
-            },
-            {
-              spotId: "id28-de-lacotedegranitrose",
-            },
-            {
-              spotId: "id29-de-menhirsdecrnac",
-            },
-          ]
-        },
         ratings: {
           create: [
             {
@@ -327,16 +304,6 @@ const userData: Prisma.UserCreateInput[] = [
         pseudo: "BriceDeNice",
         photoUrl: "url",
         gamePoint: 0,
-        favorites: {
-          create: [
-            {
-              spotId: "id1-de-dunedupilat",
-            },
-            {
-              spotId: "id17-de-gorgesduverdon",
-            },
-          ]
-        },
         ratings: {
           create: [
             {
@@ -465,16 +432,6 @@ const userData: Prisma.UserCreateInput[] = [
         pseudo: "LudovicCruchot",
         photoUrl: "url",
         gamePoint: 0,
-        favorites: {
-          create: [
-            {
-              spotId: "id15-de-lafiteria",
-            },
-            {
-              spotId: "id16-de-guethary",
-            },
-          ]
-        },
         ratings: {
           create: [
             {
@@ -584,22 +541,6 @@ const userData: Prisma.UserCreateInput[] = [
         pseudo: "GodefroyDeMontmirail",
         photoUrl: "url",
         gamePoint: 0,
-        favorites: {
-          create: [
-            {
-              spotId: "id8-de-dentellesdemontmirail",
-            },
-            {
-              spotId: "id3-de-parcnationalpyrenees",
-            },
-            {
-              spotId: "id29-de-menhirsdecrnac",
-            },
-            {
-              spotId: "id11-de-picducanigou",
-            },
-          ]
-        },
         ratings: {
           create: [
             {
@@ -732,13 +673,6 @@ const userData: Prisma.UserCreateInput[] = [
         pseudo: "MerlinEnchanteur",
         photoUrl: "url",
         gamePoint: 0,
-        favorites: {
-          create: [
-            {
-              spotId: "id10-de-foretdebroceliande",
-            },
-          ]
-        },
         ratings: {
           create: [
             {
@@ -879,119 +813,3 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
-
-// async function main() {
-//   const alice = await prisma.user.upsert({
-//     where: { email: "alice@prisma.io" },
-//     update: {},
-//     create: {
-//       email: "alice@prisma.io",
-//       password: "Alice",
-//       token: "token",
-//       role: "USER_ADMIN",
-//       profile: {
-//         create: {
-//           pseudo: "Alice231",
-//           photoUrl: "url",
-//           gamePoint: 1000,
-//           spots: {
-//             create: {
-//               name: "lac",
-//               description: "lac",
-//               isCanPark: false,
-//               isCanVisit: true,
-//               isTouristic: true,
-//               lat: 0,
-//               lng: 0,
-//               region: "Rhone-Alpes",
-//               spotPicture: {},
-//             },
-//           },
-//         },
-//       },
-//     },
-//   });
-
-// const pierre = await prisma.user.upsert({
-//     where: { email: 'pierre@prisma.io' },
-//     update: {},
-//     create: {
-//         email: 'pierre@prisma.io',
-//         password: 'Pierre',
-//         token: 'token',
-//         role: 'SIMPLE_USER',
-//     },
-// })
-
-// const jerome = await prisma.user.upsert({
-//     where: { email: 'jerome@prisma.io' },
-//     update: {},
-//     create: {
-//         email: 'jerome@prisma.io',
-//         password: 'Jerome',
-//         token: 'token',
-//         role: 'SIMPLE_USER',
-//     },
-// })
-
-//   const profilAlice = await prisma.profile.upsert({
-//     where: {},
-//     update: {},
-//     create: {
-//       userId: "1",
-//       pseudo: "Alice231",
-//       photoUrl: "url",
-//       gamePoint: 1000,
-//       spots: {
-//         create: {
-//           name: "lac",
-//           description: "lac",
-//           isCanPark: false,
-//           isCanVisit: true,
-//           isTouristic: true,
-//           lat: 0,
-//           lng: 0,
-//           region: "Rhone-Alpes",
-//           spotPicture: {},
-//         },
-//       },
-//     },
-//   });
-// }
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
-
-// itinaries: {
-//     create: [
-//       {
-//         name:'itinéraire des Alpes',
-//         description:'itinéraire des Alpes',
-//         gamePoint: 200,
-//         photoUrl: "url",
-
-//         spots: {
-//             create: {
-//                 name: 'montagne',
-//                 description: 'montagne',
-//                 rating: 4,
-//                 isCanPark: false,
-//                 isCanVisit: true,
-//                 isTouristic: false,
-//                 lat: 0,
-//                 lng: 0,
-//             },
-//         },
-//       },
-//     ],
-// },
-
-function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
